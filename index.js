7225badd626e042e9a39ce0565082694161fb175
NikitaKrutsenKo <niankr5@gmail.com>
2025-11-09 00:17:06 +0200

    lab4v3

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-export async function fetch(request) {
-  const url = new URL(request.url);
-  const name = url.searchParams.get("name") ?? "world";
-  return new Response(JSON.stringify({ hello: name }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
+addEventListener("fetch", event => {
+  event.respondWith((async () => {
+    const url = new URL(event.request.url);
+    const name = url.searchParams.get("name") ?? "world";
+    return new Response(JSON.stringify({ hello: name }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  })());
+});
